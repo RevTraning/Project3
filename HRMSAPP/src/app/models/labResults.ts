5cb5435b203af09e738a3419cb9a55d64a8217eb
Ian V <rivanarsdel@gmail.com>
2022-08-28 18:21:46 -0400

    Started API integration

--- HRMSAPP/src/app/models/labResults.ts
+++ HRMSAPP/src/app/models/labResults.ts
@@ -2,14 +2,53 @@ export class LabResults{
     
     labResultsID: number;
     apptFormID: number;
-    labAPIBody: string; //big changes incoming, depends on API
+    
+    //This gets sent to the API, needs to be concat'd into an HTTP query
+    //We do not need to send anything to populate symptoms table
+    language: string;
+    symptoms: number[];
+    DOB: number;
+    Gender: string; //req "male" "female"
+
+    //API returns a bunch, perhaps we can just take some key fields:
+    //This assume we just take the most likely issue
+    Name: string;
+    Accuracy: number;
+    Icd: string;      //Medical industry catalog for specific symptoms
+    IcdName: string;  //Name translation for you and me
+    ProfName: string; //aka "scientific name"
+    
+
+    
+
+    //API returns an array of these, "Issue" with object "Specialisation":
+
+    // [
+    //   {    
+    //     "Issue": {    
+    //       "ID": 495,    
+    //       "Name": "Bloated belly",    
+    //       "Accuracy": 90,    
+    //       "Icd": "R14",    
+    //       "IcdName": "Flatulence and related conditions",    
+    //       "ProfName": "Meteorism",    
+    //       "Ranking": 1    
+    //     },
+    
+    //     "Specialisation": [    
+    //       {    
+    //         "ID": 15,    
+    //         "Name": "General practice",    
+    //         "SpecialistID": 0    
+    //       }    
+    //     ]    
+    //   },
+
+    //Idea: iterate through Issue[] and take what we need from key fields?
+    
 
 
-  constructor(labResultsID: number = 0, labAPIBody: string = "", apptFormID?: number) {
-    this.labResultsID = labResultsID;
-    this.apptFormID = apptFormID;
-    this.labAPIBody = labAPIBody; //Changes after implementing API
-  }
+  
 
 
 }
--- HRMSAPP/src/app/services/symptom-checker-api.service.ts
+++ HRMSAPP/src/app/services/symptom-checker-api.service.ts
@@ -1,9 +1,83 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable, queueScheduler } from 'rxjs';
+import { LabResults } from '../models/labResults';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SymptomCheckerAPIService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+getSymptoms() {
+    let queryParams = new HttpParams();
+    const http = require("https");
+
+    const options = {
+	    "method": "GET",
+	    "hostname": "priaid-symptom-checker-v1.p.rapidapi.com",
+	    "port": null,
+	    "path": "/symptoms?language=en-gb&format=json",
+	    "headers": {
+		  "X-RapidAPI-Key": "60df3e7da8msh1aa6f035af42e71p1cd8eajsnf2619a4e38eb",
+		  "X-RapidAPI-Host": "priaid-symptom-checker-v1.p.rapidapi.com",
+		  "useQueryString": true
+	  }
+};
+
+    const req = http.request(options, function (res) {
+	  const chunks = [];
+    //Chunks refers to individual symptom objects, which just consist of a string Name and int ID
+
+	    res.on("data", function (chunk) {
+		    chunks.push(chunk);
+	    });
+
+	    res.on("end", function () {
+		    const body = Buffer.concat(chunks);
+		    console.log(body.toString());
+	    });
+    });
+
+  req.end();
+  console.log(req);
+  //req should be an array of objects
+  return req;
+}
+
+getDiagnosis(){
+  //todo setup query string to take patient symptoms
+  const http = require("https");
+
+  const options = {
+	  "method": "GET",
+	  "hostname": "priaid-symptom-checker-v1.p.rapidapi.com",
+	  "port": null,
+	  "path": "/diagnosis?language=en-gb&symptoms=%5B234%2C11%5D&year_of_birth=1984&gender=male&format=json",
+	  "headers": {
+		"X-RapidAPI-Key": "60df3e7da8msh1aa6f035af42e71p1cd8eajsnf2619a4e38eb",
+		"X-RapidAPI-Host": "priaid-symptom-checker-v1.p.rapidapi.com",
+		"useQueryString": true
+	  }
+  };
+
+  const req = http.request(options, function (res) {
+	const chunks = [];
+
+	  res.on("data", function (chunk) {
+		  chunks.push(chunk);
+	  });
+
+	  res.on("end", function () {
+		  const body = Buffer.concat(chunks);
+		  console.log(body.toString());
+	  });
+  });
+
+  req.end();
+
+}
+
 }
